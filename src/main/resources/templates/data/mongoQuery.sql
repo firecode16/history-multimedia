839c662db564a8a8898669ac8bb195822676034a
FireCode <69737708+firecode16@users.noreply.github.com>
2024-08-01 13:06:18 -0600

    firecode16

--- src/main/resources/templates/data/mongoQuery.sql
+++ src/main/resources/templates/data/mongoQuery.sql
@@ -1,10 +1,13 @@
 -- Open MongoDB Compass  -- query
+{ _id: ObjectId('65fc66eea669ce6c93604989') }
+{ "metadata.userId": 111101 }
+{ userId: 111101 }
 
 { files_id: ObjectId('647a2fe0709ec81cac8170da') }
 { files_id: ObjectId('647a2da8709ec81cac817074') }
 { files_id: ObjectId('647a2fe0709ec81cac8170dc') }
 
--- Open Postman
+-- Open Thunder Client or postman
 
 -- GET
 http://localhost:8081/api/multimedia/647a2da7709ec81cac816ec2
